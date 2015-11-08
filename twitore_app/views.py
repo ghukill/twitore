a085cd189e15ee1c83c31e8174bf08d48e9fb229
Graham Hukill <ghukill@gmail.com>
2015-11-08 15:09:34 -0500

    fixes to cron and search terms

--- twitore_app/views.py
+++ twitore_app/views.py
@@ -20,14 +20,14 @@ import utils
 # HTML VIEWS --------------------------------------------------------------------------------- #
 
 # index
-@app.route("{prefix}".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def index():
 
 	return render_template('index.html')
 
 
 # collections view
-@app.route("{prefix}/collections".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}/collections".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def collections():
 
 	# deal with message
@@ -43,11 +43,14 @@ def collections():
 			"name":collection.name			
 		})
 
+	logging.debug(mycron.user)
+	logging.debug(mycron.crons)
+
 	return render_template('collections.html', data=data, msg=msg)
 
 
 # single collection view
-@app.route("{prefix}/collection/<name>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}/collection/<name>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def collection(name):
 
 	# deal with message
@@ -61,11 +64,18 @@ def collection(name):
 	# stringify search terms
 	data['collection']['search_terms'] = ", ".join(data['collection']['search_terms'])
 
+	# get job cron
+	try:
+		job = mycron.find_comment('twitore_{name}'.format(name=name)).next()
+		logging.debug(job.command)
+	except:
+		logging.warning('could not find cron')
+
 	return render_template('collection.html', data=data, msg=msg)	
 
 
 # route for creating collection
-@app.route("{prefix}/create_collection".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}/create_collection".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def create_collection():
 
 	data = {}
@@ -95,18 +105,16 @@ def create_collection():
 		logging.debug("created collection %s" % c.id)
 
 		# set cron job
-		job = mycron.new(comment="twitore_%s" % collection, command="curl localhost:5001/twitore/search/%s" % collection)
-		job.minute.every(c.minute_frequency)
-		mycron.write()
+		utils.setCron(mycron,c)
 
 		# set msg in session		
 		utils.setMsg(session,"collection created","msg_success")
 
-		return redirect("{prefix}/collections".format(prefix=localConfig.twitore_app_prefix))
+		return redirect("/{prefix}/collections".format(prefix=localConfig.twitore_app_prefix))
 
 
 # route for updating collection
-@app.route("{prefix}/update_collection/<name>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}/update_collection/<name>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def update_collection(name):
 
 	data = {}
@@ -127,29 +135,37 @@ def update_collection(name):
 	logging.debug("updated collection %s" % c.id)
 
 	# update cron job
-	try:
-		job = mycron.find_comment('twitore_{name}'.format(name=name)).next()
-		job.minute.every(c.minute_frequency)
-		mycron.write()
+	# try:
+	job = mycron.find_comment('twitore_{name}'.format(name=name))
+	job = list(job)
+	logging.debug(job)
 
-		# set msg in session		
-		utils.setMsg(session,"collection updated","msg_success")
+	# update cron
+	if len(job) > 0:
+		utils.updateCron(mycron,job[0],c)
+	# set cron if not there
+	else:
+		# set cron job
+		utils.setCron(mycron,c)
 
-	except:
-		# set msg in session		
-		utils.setMsg(session,"collection could not be updated","msg_alert")
+	# set msg in session		
+	utils.setMsg(session,"collection updated","msg_success")
 
-	return redirect("{prefix}/collection/{name}".format(prefix=localConfig.twitore_app_prefix,name=name))
+	# except:
+	# 	# set msg in session		
+	# 	utils.setMsg(session,"collection could not be updated","msg_alert")
+
+	return redirect("/{prefix}/collection/{name}".format(prefix=localConfig.twitore_app_prefix,name=name))
 	
 
 # route for deleting collection
-@app.route("{prefix}/delete_collection/<collection>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
-def delete_collection(collection):
+@app.route("/{prefix}/delete_collection/<name>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+def delete_collection(name):
 
 	# retrieve collection mongo record
-	logging.debug("attempting to delete %s" % collection)
+	logging.debug("attempting to delete %s" % name)
 	try:
-		c = models.Collection.objects.get(name=collection)        
+		c = models.Collection.objects.get(name=name)        
 	except DoesNotExist:
 		logging.debug("could not find collection to delete")
 		return jsonify({'status':False})
@@ -162,21 +178,23 @@ def delete_collection(collection):
 	'''	
 
 	# remove from crontab
-	cron = list(mycron.find_comment('twitore_%s' % collection))
+	cron = list(mycron.find_comment('twitore_%s' % name))
 	logging.debug(cron)
 	if len(cron) == 1:
 		mycron.remove(cron[0])
 		mycron.write()
+	else:
+		logging.debug('could not find cron to delete')
 
 	# set msg in session		
 	utils.setMsg(session,"collection deleted","msg_success")
 
 	# if all goes well...	
-	return redirect("{prefix}/collections".format(prefix=localConfig.twitore_app_prefix))
+	return redirect("/{prefix}/collections".format(prefix=localConfig.twitore_app_prefix))
 
 
 # route for all tweets paginated
-@app.route("{prefix}/all_tweets/<int:page>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}/all_tweets/<int:page>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def all_tweets(page=1):
 
 	pt = models.MongoTweet.objects.paginate(page=page, per_page=10)
@@ -185,7 +203,7 @@ def all_tweets(page=1):
 
 
 # single collection view
-@app.route("{prefix}/collection/<name>/tweets/<int:page>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}/collection/<name>/tweets/<int:page>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def collection_tweets(name,page=1):
 
 	c = models.Collection.objects.get(name=name)
@@ -204,7 +222,7 @@ def collection_tweets(name,page=1):
 # API ROUTES --------------------------------------------------------------------------------- #
 
 # route for performing search, HTTP trigger for work
-@app.route("{prefix}/search/<collection>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+@app.route("/{prefix}/search/<collection>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 def search(collection):
 
 	'''
@@ -224,9 +242,9 @@ def search(collection):
 
 	# run search (where collection name is used for )
 	archive_dir = "/".join([localConfig.archive_directory,c.name])
-	search_terms = ",".join(c.search_terms)
-	logging.debug("Passing %s %s" % (search_terms,archive_dir))
-	archive_log = utils.search_and_archive(collection, search_terms, archive_dir)
+	# search_terms = ",".join(c.search_terms)
+	logging.debug("Passing %s %s" % (c.search_terms,archive_dir))
+	archive_log = utils.search_and_archive(collection, c.search_terms, archive_dir)
 
 
 	return jsonify({"archive_log":archive_log})
@@ -254,7 +272,7 @@ def search(collection):
 
 
 # # return json for tweet
-# @app.route("{prefix}/tweets/<limit>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+# @app.route("/{prefix}/tweets/<limit>".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 # def tweet(limit):
 
 # 	renderdict = {}
@@ -272,7 +290,7 @@ def search(collection):
 
 
 # # cron
-# @app.route("{prefix}/jobs".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
+# @app.route("/{prefix}/jobs".format(prefix=localConfig.twitore_app_prefix), methods=['GET', 'POST'])
 # def jobs():
 
 # 	# get crontab for current user
--- utils/__init__.py
+++ utils/__init__.py
@@ -21,21 +21,21 @@ searching.
 So for example if you want to search for tweets mentioning "ferguson" you can 
 run it:
 
-    % twarc-archive.py ferguson /mnt/tweets/ferguson
+	% twarc-archive.py ferguson /mnt/tweets/ferguson
 
 The first time you run this it will search twitter for tweets matching 
 "ferguson" and write them to a file:
 
-    /mnt/tweets/ferguson/tweets-0001.json
+	/mnt/tweets/ferguson/tweets-0001.json
 
 When you run the exact same command again:
 
-    % twarc-archive.py ferguson /mnt/tweets/ferguson
+	% twarc-archive.py ferguson /mnt/tweets/ferguson
 
 it will get the first tweet id in tweets-0001.json and use it to write another 
 file which includes any new tweets since that tweet:
 
-    /mnt/tweets/ferguson/tweets-0002.json
+	/mnt/tweets/ferguson/tweets-0002.json
 
 This functionality was initially part of twarc.py itself, but has been split out
 into a separate utility.
@@ -46,104 +46,119 @@ archive_file_fmt = "tweets-%04i.json"
 archive_file_pat = "tweets-(\d{4}).json$"
 
 def search_and_archive(collection, search_terms, archive_dir):
-    
-    if not os.path.isdir(archive_dir):
-        os.mkdir(archive_dir)
+	
+	if not os.path.isdir(archive_dir):
+		os.mkdir(archive_dir)
 
-    logging.info("logging search for %s to %s", search_terms, archive_dir)
+	logging.info("logging search for %s to %s", search_terms, archive_dir)
 
-    # t = twarc.Twarc(localConfig.client_key, localConfig.client_secret, localConfig.access_token, localConfig.access_token_secret)
+	last_archive = get_last_archive(archive_dir)
+	if last_archive:
+		last_id = json.loads(next(open(last_archive)))['id_str']
+		tweets = twarc_instance.search(search_terms, since_id=last_id)
+	else:
+		tweets = twarc_instance.search(search_terms)
 
-    last_archive = get_last_archive(archive_dir)
-    if last_archive:
-        last_id = json.loads(next(open(last_archive)))['id_str']
-        tweets = twarc_instance.search(search_terms, since_id=last_id)
-    else:
-        tweets = twarc_instance.search(search_terms)
+	next_archive = get_next_archive(archive_dir)
 
-    next_archive = get_next_archive(archive_dir)
+	# we only create the file if there are new tweets to save 
+	# this prevents empty archive files
+	fh = None
 
-    # we only create the file if there are new tweets to save 
-    # this prevents empty archive files
-    fh = None
+	archive_log = []
+	for tweet in tweets:
+		
+		# write to file on disk
+		if not fh:
+			fh = open(next_archive, "w")
+		logging.info("archived %s", tweet["id_str"])
+		fh.write(json.dumps(tweet))
+		fh.write("\n")
 
-    archive_log = []
-    for tweet in tweets:
-        
-        # write to file on disk
-        if not fh:
-            fh = open(next_archive, "w")
-        logging.info("archived %s", tweet["id_str"])
-        fh.write(json.dumps(tweet))
-        fh.write("\n")
+		# write to MongoDB
+		tweet['id'] = str(tweet['id']) # convert id to string
+		tweet['twitore_collection'] = collection
+		dbtweet = models.MongoTweet(**tweet)
+		dbtweet.save()                                                        
+		logging.info("tweet inserted into db, id %s" % dbtweet.id)
 
-        # write to MongoDB
-        tweet['id'] = str(tweet['id']) # convert id to string
-        tweet['twitore_collection'] = collection
-        dbtweet = models.MongoTweet(**tweet)
-        dbtweet.save()                                                        
-        logging.info("tweet inserted into db, id %s" % dbtweet.id)
+		# write to log
+		archive_log.append({
+			"id":dbtweet.id,
+			"text":dbtweet['text']
+		})
 
-        # write to log
-        archive_log.append({
-            "id":dbtweet.id,
-            "text":dbtweet['text']
-        })
+	if fh:
+		fh.close()
+	else: 
+		logging.info("no new tweets found for %s", search_terms)
 
-    if fh:
-        fh.close()
-    else: 
-        logging.info("no new tweets found for %s", search_terms)
-
-    # return archive_log
-    return archive_log
+	# return archive_log
+	return archive_log
 
 def get_last_archive(archive_dir):
-    count = 0
-    for filename in os.listdir(archive_dir):
-        m = re.match(archive_file_pat, filename)
-        if m and int(m.group(1)) > count:
-            count = int(m.group(1))
-    if count != 0:
-        return os.path.join(archive_dir, archive_file_fmt % count)
-    else:
-        return None
+	count = 0
+	for filename in os.listdir(archive_dir):
+		m = re.match(archive_file_pat, filename)
+		if m and int(m.group(1)) > count:
+			count = int(m.group(1))
+	if count != 0:
+		return os.path.join(archive_dir, archive_file_fmt % count)
+	else:
+		return None
 
 def get_next_archive(archive_dir):
-    last_archive = get_last_archive(archive_dir)
-    if last_archive:
-        m = re.search(archive_file_pat, last_archive)
-        count = int(m.group(1)) + 1
-    else:
-        count = 1
-    return os.path.join(archive_dir, archive_file_fmt % count)
+	last_archive = get_last_archive(archive_dir)
+	if last_archive:
+		m = re.search(archive_file_pat, last_archive)
+		count = int(m.group(1)) + 1
+	else:
+		count = 1
+	return os.path.join(archive_dir, archive_file_fmt % count)
 
 
 def retrieveCollection(collection):
-    try:
-        return Collection.objects.get(name=collection)        
-    except DoesNotExist:
-        return False
+	try:
+		return Collection.objects.get(name=collection)        
+	except DoesNotExist:
+		return False
 
 
 # function to deal with messages
 def msgHandle(session):
-    if 'msg' in session:
-        msg = session['msg']
-        session.pop('msg',None)
-    else:
-        msg = {}
+	if 'msg' in session:
+		msg = session['msg']
+		session.pop('msg',None)
+	else:
+		msg = {}
 
-    return msg
+	return msg
 
 
 # function to set message
 def setMsg(session, msg_text, msg_type):
-    try:
-        session['msg'] = {
-            "msg_text":msg_text,
-            "msg_type":msg_type
-        }
-        return True
-    except:
-        return False
+	try:
+		session['msg'] = {
+			"msg_text":msg_text,
+			"msg_type":msg_type
+		}
+		return True
+	except:
+		return False
+
+
+# set cron job
+def setCron(mycron, collection):
+	logging.info("setting cron for %s" % collection.name)
+
+	job = mycron.new(comment="twitore_%s" % collection.name, command="curl localhost:%s/%s/search/%s" % (localConfig.twitore_app_port,localConfig.twitore_app_prefix, collection.name) )
+	job.minute.every(collection.minute_frequency)
+	mycron.write()
+	logging.debug(mycron.crons)
+
+
+# set cron job
+def updateCron(mycron, job, collection):
+	job.minute.every(collection.minute_frequency)
+	job.command = "curl localhost:%s/%s/search/%s" % (localConfig.twitore_app_port,localConfig.twitore_app_prefix, collection.name)
+	mycron.write()
